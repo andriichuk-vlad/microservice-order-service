8a67d94f0406e68a42808672f27d57d1649ebdeb
Vladyslav <work.andriichuk.v@gmail.com>
2026-09-23 11:51:59 +0200

    integrate eureka service discovery and remove hardcoded feign url

--- src/main/java/com/example_microservice/order_service/client/ProductClient.java
+++ src/main/java/com/example_microservice/order_service/client/ProductClient.java
@@ -4,12 +4,12 @@ import com.example_microservice.order_service.dto.ProductClientDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(value = "products", url = "http://localhost:8081", fallbackFactory = ProductClientFallbackFactory.class)
+@FeignClient(name = "product-service", fallbackFactory = ProductClientFallbackFactory.class)
 public interface ProductClient {
 
     @GetMapping("/products/{productId}")
     ProductClientDto getProductById(@PathVariable("productId") Long id);
 
     @PutMapping("/products/{productId}/subtract")
-    void decreaseQuantity(@PathVariable("productId") Long id, @RequestParam Integer quantity);
+    void decreaseQuantity(@PathVariable("productId") Long productId, @RequestParam Integer quantity);
 }
